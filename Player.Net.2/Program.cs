25dc322cc2a3ae8316d93191c2d6bfb3b8c51b7d
Guy Pritchard <guy.pritchard@github.com>
2026-07-15 22:34:37 +0100

    Remove unused database projects

--- Player.Net.2/Program.cs
+++ Player.Net.2/Program.cs
@@ -2,9 +2,6 @@
 {
     using System;
     using System.Windows.Forms;
-    using DJPad.Core.Interfaces;
-    using DJPad.Core.Utils;
-    using DJPad.Db;
 
     static class Program
     {
@@ -14,8 +11,6 @@
         [STAThread]
         static void Main()
         {
-            IoCContainer.AddMapping<IMediaCorpus, SqlCeMediaCorpus>(true);
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
